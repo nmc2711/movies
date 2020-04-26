6b07c05009b895fe6852ff6c3ee347a98aaa059e
captaindevb <captain.dev.b@gmail.com>
2020-04-26 15:23:40 +0900

    기본 스타일 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">영화예매</div>;
+  return (
+    <div className="App">
+      영화예매
+      <p>css추가</p>
+    </div>
+  );
 }
 
 export default App;
